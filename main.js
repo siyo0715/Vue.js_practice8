a3edda6eb25384bb91289ea8afea7e6aa397578c
向山　翔 <hms0175km@gmail.com>
2021-12-21 17:22:40 +0900

    [Finish]Vue.js_practice8

---
+++ main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        html: '<h1>aaa</h1>',
+        url: 'http://hairskill1.com',
+        attribute: 'href',
+        aimTypingMasterObject: {
+            href: 'https://peaceful-bartik-a0e337.netlify.app/top',
+            id: 2
+        }
+    }
+})
